1ee8a138f49346549c19cdf916676e8fc0203792
Matheus-Rodd <mathrodd132@gmail.com>
2025-08-20 22:01:33 -0300

    att

--- receitas/views.py
+++ receitas/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'receitas/home.html')
+
+def receita_detail(request, receita_id):
+    # Placeholder for recipe detail view logic
+    context = {
+        'receita_id': id,
+        'receita_title': f'Receita Detalhada {id}', 
+        'receita_description': f'Descrição detalhada da receita com id {id}.',
+    }
+        # return render(request, 'receitas/receita_detail.html', context)
